232852b52e88dba646159c66325ea9630e624e30
Uzzmasaiyed <writeruzzi@gmail.com>
2024-11-19 14:41:23 +0530

    added props

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import Product from './Product'
 function App() {
   return (
     <>
-      <h1>Hello User</h1>
-      <div class="a">
-        <Product />
-        <Product />
-        <Product />
+      <h1>Welcome User!!!</h1>
+      <div className="a">
+        {/* in JSX file write className instead of class as class is a reserved keyword in JS */}
+        <Product name="Book 1" title="Harry Potter" desc="2002 Edition" />
+        <Product name="Book 2" title="Lord of the rings" desc="2010 Edition" />
+        <Product name="Book 3" title="The Vampire Diaries" desc="2008 Edition" />
       </div>
 
     </>
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,12 +1,12 @@
 import './Product.css'
 
-function Product() {
+function Product(props) {
     return (
         <>
-            <div class="prod">
-                <h1>Book</h1>
-                <h2>Harry Potter</h2>
-                <h2>2002 Edition</h2>
+            <div className="prod">
+                <h1>{props.name}</h1>
+                <h2>{props.title}</h2>
+                <h2>{props.desc}</h2>
             </div>
         </>
         //same elements are not allowed for a single component, so wrap everything in a single parent or <> can be used
