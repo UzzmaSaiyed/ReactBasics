5ce6f811ba5bf415e7f93c3595890a9150f34792
Uzzmasaiyed <writeruzzi@gmail.com>
2024-11-19 15:21:39 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css'
 import Product from './Product'
 
-function App() {
+function App(props) {
   return (
     <>
-      <h1>Welcome User!!!</h1>
+      <h1>Welcome {props.name}!!!</h1>
       <div className="a">
         {/* in JSX file write className instead of class as class is a reserved keyword in JS */}
         <Product name="Book 1" title="Harry Potter" desc="2002 Edition" />
-        <Product name="Book 2" title="Lord of the rings" desc="2010 Edition" />
-        <Product name="Book 3" title="The Vampire Diaries" desc="2008 Edition" />
+        <Product name="Book 2" title="Lord of the rings" desc="2010 Edition" detailed={{ theme: "Fantasy", Location: "Middle Earth" }} />
+        <Product name="Book 3" title="The Vampire Diaries" desc="2008 Edition" price={3000} />
+        <Product name="Book 4" title="Tom Sawyer" desc="2003 Edition" category={["Fiction", "Fantasy", "Adventure"]} />
       </div>
 
     </>
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,12 +1,21 @@
 import './Product.css'
 
 function Product(props) {
+    console.log(props)
+
+    // Destructuring props to make the code more readable and maintainable
+    let { name, title, desc, detailed, price, category } = props
+
     return (
         <>
             <div className="prod">
-                <h1>{props.name}</h1>
-                <h2>{props.title}</h2>
-                <h2>{props.desc}</h2>
+                <h1 className="b">{name}</h1>
+                <h2 className="b">{title}</h2>
+                <h2 className="b"> {desc}</h2>
+                <h2 className="b"> {detailed?.Location}</h2>
+                <h2 className="b"> {price}</h2>
+                <h2 className="b">{Array.isArray(category) && category[1]}</h2>
+
             </div>
         </>
         //same elements are not allowed for a single component, so wrap everything in a single parent or <> can be used
