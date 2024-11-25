21d46aa3c669839c2b2bd8c85ec2563f5a866c78
Uzzmasaiyed <writeruzzi@gmail.com>
2024-11-25 20:38:07 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App(props) {
         <Product name="Book 3" title="The Vampire Diaries" desc="2008 Edition" price={3000} />
         <Product name="Book 4" title="Tom Sawyer" desc="2003 Edition" category={["Fiction", "Fantasy", "Adventure"]} />
         <Product name="Book 5" title="Dr. Jekyll" desc="2007 Edition" price={9000} />
-        <Product name="Book 4" title="Tom Sawyer" desc="2003 Edition" category={["Fiction", "Fantasy", "Adventure"]} />
+        <Product name="Book 6" title="A Tale of Two Cities" desc="1998 Edition" category={["Fiction", "Polity", "Adventure"]} />
       </div>
 
       <User email="a@gmail.com" />
